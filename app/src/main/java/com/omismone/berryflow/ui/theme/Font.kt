2ae51f848c1a5533f0f3cf40d08d67b860682ae2
omismone <simone.migliazza1@gmail.com>
2026-08-31 19:40:08 +0200

    chore: initialize project

---
+++ app/src/main/java/com/omismone/berryflow/ui/theme/Font.kt
@@ -0,0 +1,11 @@
+package com.omismone.berryflow.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.omismone.berryflow.R
+
+val NunitoFontFamily = FontFamily(
+    Font(R.font.nunito_regular, FontWeight.Normal),
+    Font(R.font.nunito_medium, FontWeight.Medium)
+)
