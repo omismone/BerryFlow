183bfae5a17572bc36fe428b5330c1b741ffcb72
omismone <simone.migliazza1@gmail.com>
2026-09-19 01:14:35 +0200

    chore: bump version to 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.omismone.berryflow"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
